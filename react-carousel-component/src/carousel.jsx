862628634ed7f6b9729ba08a1a08996f0a9738d6
Jared Gilleland <ejgilleland@gmail.com>
2021-07-20 19:38:04 -0700

    Refactored single event handler into separate, simpler event handlers. Created methods to call to update the timer, instead of directly calling lifecycle methods

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,42 +4,54 @@ export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = { currentIndex: 0 };
-    this.clickHandler = this.clickHandler.bind(this);
+    this.autoStepper = this.autoStepper.bind(this);
+    this.autoResetter = this.autoResetter.bind(this);
+    this.clickLeftArrow = this.clickLeftArrow.bind(this);
+    this.clickRightArrow = this.clickRightArrow.bind(this);
+    this.clickDot = this.clickDot.bind(this);
   }
 
-  clickHandler(event) {
-    if (event.target.closest('div').classList.contains('arrow-left')) {
-      this.setState({ currentIndex: ((this.state.currentIndex + this.props.items.length) - 1) % this.props.items.length });
-      this.componentWillUnmount();
-    } else if (event.target.closest('div').classList.contains('arrow-right')) {
-      this.setState({ currentIndex: ((this.state.currentIndex + this.props.items.length) + 1) % this.props.items.length });
-      this.componentWillUnmount();
-    } else if (event.target.closest('ul').classList.contains('index')) {
-      this.setState({
-        currentIndex: (event.target.tagName === 'li')
-          ? parseInt(event.target.id, 10)
-          : parseInt(event.target.closest('li').id, 10)
-      });
-      this.componentWillUnmount();
-    }
-    this.componentDidMount();
+  clickLeftArrow(event) {
+    this.setState({ currentIndex: ((this.state.currentIndex + this.props.items.length) - 1) % this.props.items.length });
+    this.autoResetter();
+    this.autoStepper();
+  }
+
+  clickRightArrow(event) {
+    this.setState({ currentIndex: ((this.state.currentIndex + this.props.items.length) + 1) % this.props.items.length });
+    this.autoResetter();
+    this.autoStepper();
+  }
+
+  clickDot(event) {
+    this.setState({
+      currentIndex: (event.target.tagName === 'li')
+        ? parseInt(event.target.id, 10)
+        : parseInt(event.target.closest('li').id, 10)
+    });
+    this.autoResetter();
+    this.autoStepper();
   }
 
   componentDidMount() {
+    this.autoStepper();
+  }
+
+  autoStepper() {
     this.timer = setInterval(
       () => this.setState({ currentIndex: ((this.state.currentIndex + this.props.items.length) + 1) % this.props.items.length }),
       3000
     );
   }
 
-  componentWillUnmount() {
+  autoResetter() {
     clearInterval(this.timer);
   }
 
   render() {
     const keyList = this.props.items.map((element, index) => {
       return (
-        <li key={element.index} id={index}>
+        <li key={element.index} id={index} onClick={this.clickDot}>
           {(this.state.currentIndex === index)
             ? <i className="fas fa-circle"></i>
             : <i className="far fa-circle"></i>}
@@ -48,16 +60,16 @@ export default class Carousel extends React.Component {
     });
     return (
       <div className="carousel">
-        <div onClick={this.clickHandler} className="arrow-left">
+        <div onClick={this.clickLeftArrow} className="arrow-left">
           <i className="fas fa-chevron-left"></i>
         </div>
         <div className="carousel-body">
           <div className="carousel-image-container">
             <img src={this.props.items[this.state.currentIndex].source} alt={this.props.items[this.state.currentIndex].name} />
           </div>
-          <ul onClick={this.clickHandler} className='index'>{keyList}</ul>
+          <ul className='index'>{keyList}</ul>
         </div>
-        <div onClick={this.clickHandler} className="arrow-right">
+        <div onClick={this.clickRightArrow} className="arrow-right">
           <i className="fas fa-chevron-right"></i>
         </div>
       </div>
